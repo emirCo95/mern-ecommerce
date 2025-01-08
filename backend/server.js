a82ce2e0f72b71764a931cb55dab754570775d9e
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-01-08 15:28:18 -0500

    create auth routes

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,16 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+import authRoutes from './routes/auth.routes.js';
+
+dotenv.config();
 
 const app = express();
 
-app.listen(5000, () => {
-  console.log('Server is running on port 5000.');
+const PORT = process.env.PORT || 5000;
+
+app.use('/api/auth', authRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${5000}`);
 });
